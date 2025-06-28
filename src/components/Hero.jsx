6450edc875e815d26dabecda6006039ce9a1faaa
f0rk-it <mzscripterx5@gmail.com>
2025-06-28 10:12:13 +0100

    Implemented responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,22 @@
 import '../styles/hero.css'
 import CtaButton from './CtaButton'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { use, useEffect } from 'react'
 
 export default function Hero () {
+    useEffect(() => {
+            Aos.init({
+                 duration: 1000,
+                 easing: 'ease-out',
+                 once: false,
+                 offset: 120,
+                })
+        }, [])
+
     return (
         <>
-            <div className="about_content">
+            <div className="about_content" data-aos="flip-up">
                 <h1 className="title">Daily facts that make you go <span className="gradient"> 'huh'</span></h1>
                 <p className="subtitle">
                     <span>Your attention span is short. </span>
@@ -16,8 +28,8 @@ export default function Hero () {
 
             <div className="floating_facts">
                 <span className="fact_bubble top_left">Octopuses have three hearts.</span>
-                <span className="fact_bubble top_right">Bananas are berries, but strawberries aren’t.</span>
-                <span className="fact_bubble bottom_left">The Eiffel Tower can grow 6 inches in summer.</span>
+                <span className="fact_bubble top_right dont_show">Bananas are berries, but strawberries aren’t.</span>
+                <span className="fact_bubble bottom_left dont_show">The Eiffel Tower can grow 6 inches in summer.</span>
                 <span className="fact_bubble bottom_right">A bolt of lightning is 5x hotter than the sun.</span>
             </div>
         </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import '../styles/navbar.css'
 import Logo from '../assets/FactDrop.svg'
 import CtaButton from './CtaButton'
 import { useEffect, useState } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 export default function Navbar () {
     const [isScrolled, setIsScrolled] = useState(false)
@@ -14,24 +16,33 @@ export default function Navbar () {
         window.addEventListener('scroll', handleScroll)
     }, [])
 
+    useEffect(() => {
+            Aos.init({
+                 duration: 1000,
+                 easing: 'ease-out',
+                 once: false,
+                 offset: 120,
+                })
+        }, [])
+
     return (
         <nav id='navbar' className={isScrolled ? 'navbar scrolled' : 'navbar'}>
-            <div className='navbar_left'>
+            <div className='navbar_left' data-aos="fade-right">
                 <a href="/">
                     <img src={Logo} alt="FactDrop Logo" className='navbar_logo' />
                     <span className='navbar_brand'>FactDrop</span>
                 </a>
             </div>
 
-            <div className='navbar_center'>
+            <div className='navbar_center' data-aos="fade-down">
                 <a href="#about" className='navbar_link'>About</a>
                 <a href="#how_it_works" className='navbar_link'>How It Works</a>
                 <a href="#features" className='navbar_link'>Features</a>
                 <a href="#why_choose_us" className='navbar_link'>Why Choose Us</a>
             </div>
 
-            <div className='navbar_right'>
-                <CtaButton text="Start Receiving Facts" />
+            <div className='navbar_right' data-aos="fade-left">
+                <CtaButton text="Start Receiving Facts" className="navbar_btn" />
             </div>
         </nav>
     )
